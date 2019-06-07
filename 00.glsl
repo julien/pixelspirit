5e0e7488597d3035d73f729535a6bf11d8261f5c
Julien Castelain <julien.castelain@liferay.com>
2019-06-07 15:31:38 +0200

    Add example 00

---
+++ 00.glsl
@@ -0,0 +1,12 @@
+#version 300 es
+precision mediump float;
+
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform vec2 u_resolution;
+
+out vec4 outColor;
+
+void main() {
+	outColor = vec4(.0, .0, .0, 1.);
+}
