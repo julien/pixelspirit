a1048917b27c3218f57d0ebcec76c51119161524
Julien Castelain <julien.castelain@liferay.com>
2019-09-17 20:47:23 +0200

    Format JS with prettier (default settings)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import glslCanvas from './canvas.js';
+import glslCanvas from "./canvas.js";
 
-window.addEventListener('load', () => {
-	fetch('./08.glsl')
-	.then(res => res.text())
-	.then(res => {
-		glslCanvas(document.getElementById('canvas'), res);
-	});
+window.addEventListener("load", () => {
+  fetch("./08.glsl")
+    .then(res => res.text())
+    .then(res => {
+      glslCanvas(document.getElementById("canvas"), res);
+    });
 });
