40c63751bd6397663cd423d74be0028193685b3d
Julien Castelain <julien.castelain@liferay.com>
2019-09-17 20:42:59 +0200

    Use ES6 and modules

---
+++ main.js
@@ -0,0 +1,9 @@
+import glslCanvas from './canvas.js';
+
+window.addEventListener('load', () => {
+	fetch('./08.glsl')
+	.then(res => res.text())
+	.then(res => {
+		glslCanvas(document.getElementById('canvas'), res);
+	});
+});
